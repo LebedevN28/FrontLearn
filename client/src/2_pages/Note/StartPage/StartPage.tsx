378d32e762475ca5412137fbce2a58e0e0b0e714
sasha509 <abc509@yandex.ru>
2025-01-10 10:20:41 +0300

    sasha

--- client/src/2_pages/Note/StartPage/StartPage.tsx
+++ client/src/2_pages/Note/StartPage/StartPage.tsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 export default function StartPage(): React.JSX.Element {
   return (
-    <div>StartPage</div>
+    <div>
+      <div>Выбери вопросы</div>
+      
+      <div className="R7x3_ _8Iu6E" style={{ left: '-44.884px', marginBottom: '0px', marginTop: '20.3826px' }}>
+        <div className="HPdUG fF_qH" role="button" tabIndex={0}>
+          <div className="_2t1Sd Fw74a">
+            <button className="_1gEmM _7jW2t _1333i _22TV_ _3Jm09" data-test="skill-path-level-3 skill-path-level-story" aria-label="История">
+              <span className="_1kZ3q _2NI9y _1IwKR">
+                <svg width="56" height="46" viewBox="0 0 56 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M34.2346 3.25135C35.3157 2.1269 34.7053 0.276787 33.1512 0.143156C32.0512 0.0485729 30.9331 0 29.8002 0C13.342 0 0 10.2517 0 22.8979C0 26.3985 1.02236 29.7157 2.85016 32.6827C3.47761 33.7012 4.88715 33.7751 5.71626 32.9128L34.2346 3.25135Z" fill="currentColor"></path>
+                  <path d="M55.0954 12.5231C53.3548 9.61289 49.8186 6.8733 47.2219 5.21074C46.2417 4.58319 44.9772 4.77038 44.1616 5.60066C34.5035 15.4328 18.3374 31.8498 12.05 38.0427C10.9724 39.1041 10.996 40.8688 12.249 41.716C16.2271 44.4058 20.9121 45.5851 23.4852 45.9072C24.1853 45.9949 24.8657 45.7259 25.3691 45.2315C34.775 35.9934 50.2041 19.9015 54.7166 15.0879C55.3787 14.3818 55.5923 13.3539 55.0954 12.5231Z" fill="currentColor"></path>
+                </svg>
+              </span>
+              <img className="_1B6UH" draggable="false" src="https://d35aaqx5ub95lt.cloudfront.net/images/path/icons/7aa61c3f60bd961a60a46fb36e76c72f.svg" alt="История" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
-
