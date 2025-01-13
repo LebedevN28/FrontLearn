438ca5a771493f4647601048ef7b1e33e2013e0d
LebedevN28@yandex.ru <lebedevn28@yandex.ru>
2025-01-13 17:38:05 +0300

    add achivments

--- client/src/5_entities/achievement/model/achievement.schema.ts
+++ client/src/5_entities/achievement/model/achievement.schema.ts
@@ -4,4 +4,7 @@ export const achievementSchema = z.object({
   id: z.number(),
   title: z.string().min(1, 'Title is required'), // Название достижения
   description: z.string().optional(), // Описание достижения
+  type: z.enum(['level', 'questions', 'modules', 'daily', 'progress', 'streak', 'speed', 'custom']), // Тип достижения
+  criteria: z.string().min(1, 'Criteria is required'), // Условие в формате JSON
+  points: z.number().nonnegative().optional(), // Очки за достижение
 });
