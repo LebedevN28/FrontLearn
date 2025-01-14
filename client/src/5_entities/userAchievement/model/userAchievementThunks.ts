94bb96ca85234414009c2905996d8345f028c61f
bilberian <bilberian@mail.ru>
2025-01-14 18:29:08 +0300

    minor

--- client/src/5_entities/userAchievement/model/userAchievementThunks.ts
+++ client/src/5_entities/userAchievement/model/userAchievementThunks.ts
@@ -7,7 +7,7 @@ export const saveUserAchievements = createAsyncThunk(
   async ({ userId, achievements }: { userId: number; achievements: number[] }) => {
     try {
       await UserAchievementsService.saveUserAchievements(userId, achievements);
-      console.log(achievements, 'achievements');
+ 
 
       return achievements; // Возвращаем список сохраненных достижений
     } catch (error) {
--- server/src/controllers/achievement.controller.js
+++ server/src/controllers/achievement.controller.js
@@ -4,8 +4,6 @@ module.exports = {
   async getAllAchievements(req, res) {
     try {
       const achievements = await achievementService.getAllAchievements();
-      console.log(achievements, 'achievements');
-      
       res.status(200).json(achievements);
     } catch (error) {
       console.error(error);
