51b49ce6d88b13f5850e7f161105ebeae08944a5
sasha509 <abc509@yandex.ru>
2025-01-15 13:28:56 +0300

    sorting succs

--- server/src/services/task.service.js
+++ server/src/services/task.service.js
@@ -9,11 +9,11 @@ module.exports = {
 
   /** ****  7c864eb7-e22d-4493-bfc4-8f53851c54a6  ****** */
   async getAllTasks() {
-    return  Task.findAll();
+    return Task.findAll();
   },
 
   async getTaskById(id) {
-    return  Task.findByPk(id);
+    return Task.findByPk(id);
   },
 
   // async createTask(taskData) {
@@ -33,10 +33,19 @@ module.exports = {
   //   return task;
   // },
 
-  async getTaskByModuleId(moduleId) {
+  async getTaskByModuleId(moduleId, difficulty) {
+    if (!difficulty) {
+      const task = await Task.findAll({
+        where: {
+          moduleId,
+        },
+      });
+      return task;
+    }
     const task = await Task.findAll({
       where: {
         moduleId,
+        difficulty,
       },
     });
     return task;
