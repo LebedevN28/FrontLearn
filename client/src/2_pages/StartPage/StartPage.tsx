282c535268edd38a682f9f1a56852b62a7940951
Mortek1 <mortek90@mail.ru>
2025-01-16 10:37:01 +0300

    some stylez

--- client/src/2_pages/StartPage/StartPage.tsx
+++ client/src/2_pages/StartPage/StartPage.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 import { useAppSelector } from '../../6_shared/lib/hooks';
-import styles from './StartPage.module.css'; 
+import styles from './StartPage.module.css';
 import PhaseCard from '../../4_features/PhaseCard/PhaseCard';
 
 export default function StartPage(): React.JSX.Element {
   const modules = useAppSelector((store) => store.modules.modules);
 
   return (
-    <div>
-      <h1>Выбери Фазу</h1>
-      <div className={styles['phase-card-container']}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>Выбери Фазу</h1>
+      <div className={styles.phaseCardContainer}>
         {modules.map((module) => (
           <PhaseCard key={module.id} module={module} />
         ))}
       </div>
+      <img
+        src="/imgs/startpage.jpg"
+        alt="Start Page"
+        className={styles.image}
+      />
     </div>
   );
 }
